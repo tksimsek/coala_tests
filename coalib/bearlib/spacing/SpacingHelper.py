8f9874d4bcac765a98334bbd12c7d5a2cff1ca48
Midori072 <malijoe@hotmail.co.uk>
2024-06-26 21:43:33 +0200

    First functional branch test implemented

--- coalib/bearlib/spacing/SpacingHelper.py
+++ coalib/bearlib/spacing/SpacingHelper.py
@@ -1,30 +1,30 @@
-from coalib.bearlib.abstractions.SectionCreatable import SectionCreatable
-from coala_utils.decorators import enforce_signature
+#from coalib.bearlib.abstractions.SectionCreatable import SectionCreatable
+#from coala_utils.decorators import enforce_signature
 
 
-class SpacingHelper(SectionCreatable):
+""" class SpacingHelper(SectionCreatable):
     DEFAULT_TAB_WIDTH = 4
 
     def __init__(self, tab_width: int = DEFAULT_TAB_WIDTH):
-        """
+        """"""
         Creates a helper object for spacing operations.
 
         :param tab_width: The number of spaces which visually equals a tab.
-        """
+        """"""
         SectionCreatable.__init__(self)
         if not isinstance(tab_width, int):
             raise TypeError("The 'tab_width' parameter should be an integer.")
 
         self.tab_width = tab_width
 
-    @enforce_signature
+    #@enforce_signature
     def get_indentation(self, line: str):
-        """
+        """"""
         Checks the lines indentation.
 
         :param line: A string to check for indentation.
         :return:     The indentation count in spaces.
-        """
+        """"""
         count = 0
         for char in line:
             if char == ' ':
@@ -39,9 +39,9 @@ class SpacingHelper(SectionCreatable):
 
         return count
 
-    @enforce_signature
+    #@enforce_signature
     def replace_tabs_with_spaces(self, line: str):
-        """
+        """"""
         Replaces tabs in this line with the appropriate number of spaces.
 
         Example: " \t" will be converted to "    ", assuming the tab_width is
@@ -49,20 +49,20 @@ class SpacingHelper(SectionCreatable):
 
         :param line: The string with tabs to replace.
         :return:     A string with no tabs.
-        """
+        """"""
         for t_position, t_length in sorted(self.yield_tab_lengths(line),
                                            reverse=True):
             line = line[:t_position] + t_length * ' ' + line[t_position+1:]
 
         return line
 
-    @enforce_signature
+    #@enforce_signature
     def yield_tab_lengths(self, input: str):
-        """
+        """"""
         Yields position and size of tabs in a input string.
 
         :param input: The string with tabs.
-        """
+        """"""
         tabless_position = 0
         for index, char in enumerate(input):
             if char == '\t':
@@ -73,6 +73,7 @@ class SpacingHelper(SectionCreatable):
                 continue
 
             tabless_position += 1
+"""
 
 branch_coverage = {
     "replace_spaces_with_tabs_1": False,  # First if statement (char == ' ')
@@ -88,52 +89,43 @@ class TextProcessor:
     def __init__(self, tab_width):
         self.tab_width = tab_width
 
-    @enforce_signature
     def replace_spaces_with_tabs(self, line: str):
-        """
-        Replaces spaces with tabs where possible. However in no case only one
-        space will be replaced by a tab.
-
-        Example: " \t   a_text   another" will be converted to
-        "\t   a_text\tanother", assuming the tab_width is set to 4.
-
-        :param line: The string with spaces to replace.
-        :return:     The converted string.
-        """
         global branch_coverage
         
         currspaces = 0
         result = ''
-        # Tracking the index of the string isn't enough because tabs are
-        # spanning over multiple columns
         tabless_position = 0
+
         for char in line:
             if char == ' ':
                 currspaces += 1
                 tabless_position += 1
                 branch_coverage["replace_spaces_with_tabs_1"] = True
             elif char == '\t':
-                space_count = (self.tab_width - tabless_position % self.tab_width)
+                space_count = self.tab_width - (tabless_position % self.tab_width)
                 currspaces += space_count
                 tabless_position += space_count
                 branch_coverage["replace_spaces_with_tabs_2"] = True
             else:
-                result += currspaces * ' ' + char
+                if currspaces == 1 and tabless_position % self.tab_width == 1:
+                    result += '\t'  # Convert single space to tab if it aligns
+                    branch_coverage["replace_spaces_with_tabs_5"] = True
+                elif currspaces > 1 and currspaces % self.tab_width == 0:
+                    result += '\t'  # Convert multiple spaces to a tab if they align
+                    branch_coverage["replace_spaces_with_tabs_6"] = True
+                else:
+                    result += currspaces * ' ' + char
+                    branch_coverage["replace_spaces_with_tabs_3"] = True
                 currspaces = 0
                 tabless_position += 1
-                branch_coverage["replace_spaces_with_tabs_3"] = True
 
-            # tabless_position is now incremented to point _after_ the current
-            # char
             if tabless_position % self.tab_width == 0 and currspaces:
-                branch_coverage["replace_spaces_with_tabs_4"] = True
                 if currspaces == 1 and char == ' ':
-                    result += ' '
+                    result += ' '  # Append single space as is
                     branch_coverage["replace_spaces_with_tabs_5"] = True
                 else:
-                    result += '\t'
+                    result += '\t'  # Convert multiple spaces to a tab if they align
                     branch_coverage["replace_spaces_with_tabs_6"] = True
-
                 currspaces = 0
 
         result += currspaces * ' '
--- coalib/bearlib/spacing/test_SpacingHelper.py
+++ coalib/bearlib/spacing/test_SpacingHelper.py
@@ -5,7 +5,7 @@ def test_replace_spaces_with_tabs_branch_coverage():
 
     # Test cases to cover each branch
     test_cases = [
-        (" ", "\t", {
+        (" ", " ", {
             "replace_spaces_with_tabs_1": True,  # First if statement (char == ' ')
         }),
         ("\t", "\t", {
@@ -20,17 +20,20 @@ def test_replace_spaces_with_tabs_branch_coverage():
         }),
         ("  a", "\t  a", {
             "replace_spaces_with_tabs_4": True,  # If statement for tab alignment (tabless_position % self.tab_width == 0)
-            "replace_spaces_with_tabs_6": True,  # Else nested in tab alignment
+            "replace_spaces_with_tabs_5": True,  # If nested in tab alignment (currspaces == 1 and char == ' ')
         }),
+
         ("", "", {
             "replace_spaces_with_tabs_7": True,  # Final appending of remaining spaces
         }),
     ]
 
+
     for i, (input_str, expected_output, expected_coverage) in enumerate(test_cases):
         result = processor.replace_spaces_with_tabs(input_str)
+        print(f"DEBUG: Result {i+1} = '{result}', Expected = '{expected_output}'")
         assert result == expected_output, f"Test case {i+1} failed: {result} != {expected_output}"
-        
+
         # Check branch coverage
         for branch_name, expected_status in expected_coverage.items():
             assert branch_coverage[branch_name] == expected_status, f"Branch {branch_name} not covered as expected."
